5c971632065d19679109c48d7c9122c8e232d5c8
Alex <eternity.2019@mail.ru>
2021-02-01 23:52:57 +0300

    Added Texture2D class

---
+++ src/Renderer/Texture2D.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace Renderer {
+
+	class Texture2D
+	{
+	public:
+		Texture2D(const GLuint width, GLuint heigth, 
+				  const unsigned char* data, 
+				  const unsigned int channels,
+				  const GLenum filter, 
+				  const GLenum wrapMode);
+		Texture2D() = delete;
+		Texture2D(const Texture2D&) = delete;
+		Texture2D& operator=(const Texture2D&) = delete;
+		Texture2D& operator=(Texture2D&& texture2d);
+		Texture2D(Texture2D&& texture2d);
+		~Texture2D();
+
+		void bind() const;
+		
+	private:
+		GLuint m_ID;
+		GLenum m_mode;
+		unsigned int m_width;
+		unsigned int m_heigth;
+	};
+}
--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -1,5 +1,6 @@
 #include "ResourceManager.h"
 #include "../Renderer/ShaderProgram.h"
+#include "../Renderer/Texture2D.h"
 #include <sstream>
 #include <fstream>
 #include <iostream>
@@ -47,7 +48,7 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const
 	std::cerr<< "Cant find the shader program: " << shaderName << std::endl;
 	return nullptr;
 }
-void ResourceManager::loadTexture(const std::string& textureName, const std::string texturePath)
+std::shared_ptr<Renderer::Texture2D> ResourceManager::loadTexture(const std::string& textureName, const std::string texturePath)
 {
 	int channels = 0;
 	int width = 0;
@@ -57,9 +58,22 @@ void ResourceManager::loadTexture(const std::string& textureName, const std::str
 	if (!pixels)
 	{
 		std::cerr << "Cant load image: " << texturePath << std::endl;
-		return;
+		return nullptr;
 	}
+	std::shared_ptr<Renderer::Texture2D> newTexture = m_textures.emplace(textureName, std::make_shared<Renderer::Texture2D>(width, heigth, pixels, channels, GL_NEAREST,GL_CLAMP_TO_EDGE)).first->second;
+	return newTexture;
 	stbi_image_free(pixels);
+}
+std::shared_ptr<Renderer::Texture2D> ResourceManager::getTexture(const std::string& textureName)
+{
+	TexturesMap::const_iterator it = m_textures.find(textureName);
+	
+	if (it != m_textures.end())
+	{
+		return it->second;
+	}
+	std::cerr << "Cant find the texture: " << textureName << std::endl;
+	return nullptr;
 }
 std::string ResourceManager::getFileString(const std::string& relativeFilePath) const
 {
